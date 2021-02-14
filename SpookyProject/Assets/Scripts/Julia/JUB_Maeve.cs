303e011ff90f5e0f71f5c267c4045ee4075b530c
Julia <bourdinjulia@yahoo.com>
2021-02-14 16:42:25 +0100

    Add small and heavy attacks to Maeve
    Some layers to handle it too and ennemies damage script

--- SpookyProject/Assets/Scripts/Julia/JUB_Maeve.cs
+++ SpookyProject/Assets/Scripts/Julia/JUB_Maeve.cs
@@ -2,6 +2,7 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 
 public class JUB_Maeve : MonoBehaviour
@@ -10,6 +11,7 @@ public class JUB_Maeve : MonoBehaviour
     public JUB_MaeveCollisionDetector left, right, top, bottom;
     public bool collisionLeft, collisionRight, collisionTop, collisionBottom;
 
+    //mouvements
     Rigidbody2D rigidBody;
     Controller controller;
     public float speed, rollSpeed, rollDuration, rollRecover, crouchSpeed, xVelocity, yVelocity, accelerationTime, immunityTime;
@@ -17,6 +19,17 @@ public class JUB_Maeve : MonoBehaviour
 
     [SerializeField]
     bool isInRoll, isCrouching, isInRecoil, isInImmunity, isInRecover;
+    [SerializeField]
+    bool attackMaintained, isInBuildup, isInAttack;
+
+    //combat
+    public Transform attackPoint;
+    public float attackRange, attackTime, attackAngle;
+    public Vector2 quickAtkZone, heavyAtkZone;
+    public LayerMask ennemies, breakableObjects;
+    public int attackDamage;
+    public bool ennemyWasHitOnce;
+    List<Collider2D> ennemiesHitLastTime;
     
     // Start is called before the first frame update
     void Start()
@@ -25,7 +38,13 @@ public class JUB_Maeve : MonoBehaviour
         controller = new Controller();
         controller.Enable();
 
+        AttackProfile quickAttack = new AttackProfile(1, new Vector2(1, 1),0.1f, 0.2f);
+        AttackProfile heavyAttack = new AttackProfile(3, new Vector2(2, 1),0, 0.8f);
+
         controller.MainController.Roll.performed += ctx => Roll();
+        controller.MainController.Attack.performed += ctx => Attack(quickAttack);// Attack();
+        controller.MainController.HeavyAttack.performed += ctx => Attack(heavyAttack);
+        //controller.MainController.Crouch.performed += ctx => isCrouching = !isCrouching;
     }
 
     // Update is called once per frame
@@ -142,4 +161,98 @@ public class JUB_Maeve : MonoBehaviour
         yield return new WaitForSeconds(immunityTime);
         isInImmunity = false;
     }
+
+    void Attack(AttackProfile attackProfile)
+    {
+        if (!isInRecover && !isInBuildup && !isInRoll && !isCrouching)
+        {
+            ennemiesHitLastTime.Clear();
+            isInBuildup = true;
+            StartCoroutine(Buildup(attackProfile));
+        }
+    }
+
+    IEnumerator Buildup(AttackProfile attackProfile)
+    {
+        yield return new WaitForSeconds(attackProfile.atkBuildup);
+        isInBuildup = false;
+        isInRecover = true;
+        StartCoroutine(Hit(attackProfile));
+    }
+    IEnumerator Hit(AttackProfile attackProfile)
+    { 
+        Collider2D[] hitEnnemies = Physics2D.OverlapCircleAll(transform.position, attackProfile.atkZone.x, ennemies);
+        Collider2D[] hitObjects = Physics2D.OverlapCircleAll(attackPoint.position, attackProfile.atkZone.x, breakableObjects);
+
+        
+        if(!ennemyWasHitOnce)
+        {
+            foreach(Collider2D ennemy in hitEnnemies)
+            {
+                if (!ennemiesHitLastTime.Contains(ennemy))
+                {
+                    ennemy.GetComponent<JUB_EnnemyDamage>().TakeDamage(attackProfile.atkDamage);
+                    Debug.Log("attack was performed");
+                    ennemyWasHitOnce = true;
+                    ennemiesHitLastTime.Add(ennemy);
+                }
+            }
+
+        }
+
+        foreach(Collider2D breakableObject in hitObjects)
+        {
+
+        }
+
+        yield return new WaitForSeconds(attackProfile.atkRecover);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, quickAtkZone.x);
+        Vector3 attackLength;
+        attackLength = new Vector3(quickAtkZone.x, 0);
+        attackLength = Quaternion.Euler(0, 0, quickAtkZone.y) * attackLength;
+        Gizmos.DrawLine(transform.position, transform.position + attackLength);
+        attackLength = Quaternion.Euler(0, 0, -2 * quickAtkZone.y) * attackLength;
+        Gizmos.DrawLine(transform.position, transform.position + attackLength);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, heavyAtkZone.x);
+        attackLength = new Vector3(heavyAtkZone.x, 0);
+        attackLength = Quaternion.Euler(0, 0, heavyAtkZone.y) * attackLength;
+        Gizmos.DrawLine(transform.position, transform.position + attackLength);
+        attackLength = Quaternion.Euler(0, 0, -2 * heavyAtkZone.y) * attackLength;
+        Gizmos.DrawLine(transform.position, transform.position + attackLength);
+    }
+
+    class AttackProfile
+    {
+        public AttackProfile(float damage, Vector2 zone, float buildup, float recover)
+        {
+            atkDamage = damage;
+            atkZone = zone;
+            atkRecover = recover;
+            atkBuildup = buildup;
+        }
+
+        public float atkDamage, atkRecover, atkBuildup;
+        public Vector2 atkZone;
+
+        public void ChangeDamage(float changeAmount)
+        {
+            atkDamage += changeAmount;
+        }
+
+        public void NewDamage(float newDamageValue)
+        {
+            atkDamage = newDamageValue;
+        }
+    }
+
 }
